18fc929923bae0c38200c02e2e07dc7966b82606
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-14 12:10:18 -0500

    Update variables practice

--- variables.py
+++ variables.py
@@ -1,9 +1,13 @@
 name = "Sai"
-age = 25
-height = 5.8
+age = 24
+city = "Texas"
 is_student = True
 
 print("Name:", name)
+print("City:", city)
 print("Age:", age)
-print("Height:", height)
-print("Student:", is_student)
+print("Student status:", is_student)
+
+# updating value
+age = age + 1
+print("Updated age:", age)
