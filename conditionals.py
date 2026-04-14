8419733ed39ee1925d714eebc0f6c3734dc3580f
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-14 12:20:10 -0500

    Add conditional examples

---
+++ conditionals.py
@@ -0,0 +1,28 @@
+# simple conditional examples
+
+price = 120
+
+if price > 150:
+    print("Very expensive")
+elif price > 100:
+    print("Moderate price")
+else:
+    print("Low price")
+
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+else:
+    print("Need improvement")
+
+
+is_active = True
+
+if is_active:
+    print("Account is active")
+else:
+    print("Account is inactive")
