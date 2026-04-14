113fea5fb8166a35b0882292fbc8e42032f45444
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-14 12:22:51 -0500

    Add loop examples

---
+++ loops.py
@@ -0,0 +1,39 @@
+# -------- FOR LOOP EXAMPLES --------
+
+# loop through a list
+items = ["pen", "book", "bottle"]
+
+for item in items:
+    print("Item:", item)
+
+
+# loop using range
+for i in range(5):
+    print("Count:", i)
+
+
+# loop with condition (even numbers)
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    if number % 2 == 0:
+        print("Even number:", number)
+
+
+
+# -------- WHILE LOOP EXAMPLES --------
+
+# basic while loop
+count = 0
+
+while count < 3:
+    print("While count:", count)
+    count = count + 1
+
+
+# while loop with condition
+num = 1
+
+while num <= 5:
+    print("Number:", num)
+    num += 1
