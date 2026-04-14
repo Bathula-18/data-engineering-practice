f427d26bac48936e07c4aab9df6187ef942feb9a
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-14 17:45:24 -0500

    Add collections practice

---
+++ collections.py
@@ -0,0 +1,19 @@
+# practice of python collections
+
+# list example (ordered, changeable)
+fruits = ["apple", "banana", "mango"]
+print("Fruits list:", fruits)
+
+# set example (no duplicates)
+numbers = {1, 2, 3, 3, 4}
+print("Unique numbers:", numbers)
+
+# dictionary example (key-value pairs)
+student = {
+    "name": "Sai",
+    "age": 24,
+    "city": "Texas"
+}
+
+print("Student name:", student["name"])
+print("Student city:", student["city"])
