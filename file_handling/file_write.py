ecdc7f89fa3e3c72859698d48eff56ebcaf545a4
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-14 21:14:35 -0500

    Add file writing example

---
+++ file_handling/file_write.py
@@ -0,0 +1,7 @@
+# practice of writing to a file
+
+with open("output.txt", "w") as file:
+    file.write("This is file writing practice.\n")
+    file.write("Learning Python file handling.")
+
+print("File written successfully")
