33f08dc9a433477722a8f4236357f55accdf87e0
Bathula-18 <saihemanthbathula36@gmail.com>
2026-04-30 20:02:58 -0500

    Add deduplication using row_number

---
+++ week3_sql_advanced/dedup_logic.sql
@@ -0,0 +1,12 @@
+-- Latest record per customer
+
+SELECT *
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY updated_at DESC
+           ) AS rn
+    FROM customers
+) t
+WHERE rn = 1;
